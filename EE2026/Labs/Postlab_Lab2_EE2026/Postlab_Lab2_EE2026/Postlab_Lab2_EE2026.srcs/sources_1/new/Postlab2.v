04c3d0425f637eeaea580f513ba6d33fc3ee719e
Dennis <denniswong178@gmail.com>
2024-02-20 22:56:14 +0800

    Basys3: Added Postlab Assignment 2

---
+++ EE2026/Labs/Postlab_Lab2_EE2026/Postlab_Lab2_EE2026/Postlab_Lab2_EE2026.srcs/sources_1/new/Postlab2.v
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+// Create Date: 02/20/2024 10:14:11 PM
+// Matric No. A0248636U
+
+module Top_Module(
+    );
+endmodule
---
+++ EE2026/Labs/Postlab_Lab2_EE2026/Postlab_Lab2_EE2026/Postlab_Lab2_EE2026.srcs/sources_1/new/five_bit_adder.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+// Create Date: 02/20/2024 10:17:13 PM
+
+module five_bit_adder( input [4:0]A, input [4:0]B, output [4:0]S , output Carry_out);
+    
+    reg Carry_in = 0;
+    wire [3:0] Carry_out_within;
+    
+    one_bit_adder adder0 (A[0], B[0], Carry_in, S[0], Carry_out_within[0]);
+    one_bit_adder adder1 (A[1], B[1], Carry_out_within[0], S[1], Carry_out_within[1]);
+    one_bit_adder adder2 (A[2], B[2], Carry_out_within[1], S[2], Carry_out_within[2]);
+    one_bit_adder adder3 (A[3], B[3], Carry_out_within[2], S[3], Carry_out_within[3]);
+    one_bit_adder adder4 (A[4], B[4], Carry_out_within[3], S[4], Carry_out);
+    
+endmodule
