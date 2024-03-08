c4a9898e81085e4dbebbc7ae49d9e5073fafff1b
Dennis <denniswong178@gmail.com>
2024-03-08 23:50:22 +0800

    EE2026 Completed Postlab3

--- EE2026/Labs/Postlab_Lab3_EE2026/Postlab_Lab3_EE2026.srcs/sources_1/new/Postlab3.v
+++ EE2026/Labs/Postlab_Lab3_EE2026/Postlab_Lab3_EE2026.srcs/sources_1/new/Postlab3.v
@@ -5,8 +5,8 @@ module Postlab3_Top(input CLOCK,
                     input btnR, input btnU, input btnL, input btnC, input [2:0] sw, 
                     output reg [6:0]seg=7'b111_1111, output reg dp = 1'b1, output reg [3:0]an=4'b1111,
                     output reg unlocked_led = 1'b0,                                  // for subtask C
-                    output reg led_14, output reg led_13, output reg led_12,        // for subtask C testing
-                    output subtask_D_led,                                            // for subtask D tsting
+//                  output reg led_14, output reg led_13, output reg led_12,        // for subtask C testing
+//                  output subtask_D_led,                                            // for subtask D tsting
                     output reg [8:0]LED=9'b0_0000_0000);
     
 reg [31:0] count_100_111hz = 0; // counter for the 100/111 hz clock
@@ -27,9 +27,10 @@ wire master_stop;          // Wire Instatiation for Subtask D
 clk_1hz clk0 (CLOCK, clock_1hz_status, clock_1hz_status_count);                             // 1hz clock inst.
 clk_10hz clk1 (CLOCK, clock_10hz_status, clock_10hz_status_count);                          // 10hz clock inst.
 clk_100hz clk2 (CLOCK, clock_100hz_status, clock_100hz_status_count);                       // 100hz clock inst. 
-clk_1hz_subtaskD clk3 (CLOCK, master_switch, subtask_D_led);                                // Clock instatiations for Subtask D
+clk_1hz_subtaskD clk3 (CLOCK, master_switch, master_stop); //subtask_D_led);                                // Clock instatiations for Subtask D
     
  // Subtask C -- tracks the current symbol
+ reg  [1:0] current_symbol = 0; 
  
 /* ------------------- Subtask A -------------------*/
 // Main Clock of the Program - - Anytime master_switch is on, the count increment stops
@@ -75,17 +76,21 @@ begin
                         default: LED <= 9'b1_1111_1111;
                     endcase
                 end
-        else
-            begin
-                if(sw[2]==1)
-                    begin LED[8:3]<= 6'b1111_11; LED[2] <= clock_100hz_status; LED[1:0] <= 2'b11; end
-                else if (sw[1]==1)
-                    begin LED[8:2]<= 7'b1111_111; LED[1] <= clock_10hz_status; LED[0] <= 1'b1;end
-                else if (sw[0]==1)
-                    begin LED[8:1]<= 8'b1111_1111; LED[0] <= clock_1hz_status; end
-                else
-                    begin LED <= 9'b1_1111_1111; end                       
-            end     
+                
+             else if(subtaskA_done_flag == 1 & unlocked_led == 0)
+                begin LED <= 9'b1_1111_1111; end //
+                
+             else
+                    begin
+                        if(sw[2]==1)
+                            begin LED[8:3]<= 6'b1111_11; LED[2] <= clock_100hz_status; LED[1:0] <= 2'b11; end
+                        else if (sw[1]==1)
+                            begin LED[8:2]<= 7'b1111_111; LED[1] <= clock_10hz_status; LED[0] <= 1'b1;end
+                        else if (sw[0]==1)
+                            begin LED[8:1]<= 8'b1111_1111; LED[0] <= clock_1hz_status; end
+                        else
+                            begin LED <= 9'b1_1111_1111; end       
+                    end                 
         end
 end
 
@@ -157,78 +162,93 @@ if(master_stop != 1)
     end end
 end
 
+// if Master Switch is on
 always @ (master_switch or unlocked_led or segment_count or segment_count_L or segment_count_U or segment_count_R) begin
-    if(subtaskA_done_flag == 1 & unlocked_led == 0)
+    if(master_stop == 1) 
         begin
-            if(segment_count == 0 & segment_count_L == 0 & segment_count_U == 0 & segment_count_R == 0 )
-                begin an <= 4'b1110; seg <= 7'b010_0111; end                                                    // c
-            else if(segment_count == 1 & segment_count_L == 0 & segment_count_U == 0 & segment_count_R == 0)
-                begin an <= 4'b1101; seg <= 7'b100_1111; end                                                    // l 
-            else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 0 & segment_count_R == 0)
-                begin an <= 4'b1011; seg <= 7'b110_0011; end                                                    // u
-            else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 0)
-                begin an <= 4'b0111; seg <= 7'b010_1111; end                                                    // r
-            else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 1)
-                begin an <= 4'b1110; seg <= 7'b010_0111; end                                                    // c
-            else if(segment_count == 2 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 1)
-                begin an <= 4'b1101; seg <= 7'b100_1111; end                                                    // l 
-            else if(segment_count == 2 & segment_count_L == 2 & segment_count_U == 1 & segment_count_R == 1)
-                begin an <= 4'b1110; seg <= 7'b010_0111; unlocked_led <= 1; end                                 // c
+            begin an <= 4'b0000; seg <= 7'b100_0001; end
         end
-    else
+        
+    else 
         begin
-            if (subtaskA_done_flag == 1) // 
+            if (subtaskA_done_flag == 0)
+                begin an <= 4'b1111; seg <= 7'b111_1111; end  // edge case: unlocked_led ==0, but master_switch on then offend         
+                  
+            else if(subtaskA_done_flag == 1 & unlocked_led == 0)
                 begin
-                    if(sw[2]==1)
+                    if(segment_count == 0 & segment_count_L == 0 & segment_count_U == 0 & segment_count_R == 0 )
+                        begin an <= 4'b1110; seg <= 7'b010_0111; end                                                    // c
+                    else if(segment_count == 1 & segment_count_L == 0 & segment_count_U == 0 & segment_count_R == 0)
+                        begin an <= 4'b1101; seg <= 7'b100_1111; end                                                    // l 
+                    else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 0 & segment_count_R == 0)
+                        begin an <= 4'b1011; seg <= 7'b110_0011; end                                                    // u
+                    else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 0)
+                        begin an <= 4'b0111; seg <= 7'b010_1111; end                                                    // r
+                    else if(segment_count == 1 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 1)
+                        begin an <= 4'b1110; seg <= 7'b010_0111; end                                                    // c
+                    else if(segment_count == 2 & segment_count_L == 1 & segment_count_U == 1 & segment_count_R == 1)
+                        begin an <= 4'b1101; seg <= 7'b100_1111; end                                                    // l 
+                    else if(segment_count == 2 & segment_count_L == 2 & segment_count_U == 1 & segment_count_R == 1)
+                        begin an <= 4'b1110; seg <= 7'b010_0111; unlocked_led <= 1; end                                 // c
+                end
+                
+            else
                         begin
-                            case(clock_100hz_status_count)
-                                0: begin an <= 4'b1110; seg <= 7'b010_0111; end // c
-                                1: begin an <= 4'b1101; seg <= 7'b100_1111; end // l 
-                                2: begin an <= 4'b1011; seg <= 7'b110_0011; end // u
-                                default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signlal
-                            endcase
-                        end
-                    else if(sw[1]==1)
-                        begin
-                            case(clock_10hz_status_count)
-                                0: begin an <= 4'b1110; seg <= 7'b010_0111; end // c
-                                1: begin an <= 4'b1101; seg <= 7'b100_1111; end // l 
-                                2: begin an <= 4'b1011; seg <= 7'b110_0011; end // u
-                                default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signlal
-                            endcase 
-                         end
-                    else if(sw[0]==1)
-                        begin
-                            case(clock_1hz_status_count)
-                                0: begin an <= 4'b1110; seg <= 7'b010_0111; end // c
-                                1: begin an <= 4'b1101; seg <= 7'b100_1111; end // l 
-                                2: begin an <= 4'b1011; seg <= 7'b110_0011; end // u
-                                default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signlal
-                            endcase 
-                        end    
-                    else
-                        begin 
-                            an <= 4'b1110; seg <= 7'b010_0111;
-                        end // c
-            end
+                            if(sw[2]==1)
+                                begin
+                                    case(clock_100hz_status_count)
+                                        0: begin an <= 4'b1110; seg <= 7'b010_0111; current_symbol <= 0; end // c
+                                        1: begin an <= 4'b1101; seg <= 7'b100_1111; current_symbol <= 1;end // l 
+                                        2: begin an <= 4'b1011; seg <= 7'b110_0011; current_symbol <= 2;end // u
+                                        default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signal
+                                    endcase
+                                end
+                            else if(sw[1]==1)
+                                begin
+                                    case(clock_10hz_status_count)
+                                         0: begin an <= 4'b1110; seg <= 7'b010_0111; current_symbol <= 0; end // c
+                                        1: begin an <= 4'b1101; seg <= 7'b100_1111; current_symbol <= 1;end // l 
+                                        2: begin an <= 4'b1011; seg <= 7'b110_0011; current_symbol <= 2;end // u
+                                        default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signal
+                                    endcase 
+                                 end
+                            else if(sw[0]==1)
+                                begin
+                                    case(clock_1hz_status_count)
+                                         0: begin an <= 4'b1110; seg <= 7'b010_0111; current_symbol <= 0; end // c
+                                        1: begin an <= 4'b1101; seg <= 7'b100_1111; current_symbol <= 1;end // l 
+                                        2: begin an <= 4'b1011; seg <= 7'b110_0011; current_symbol <= 2;end // u
+                                        default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signal
+                                    endcase 
+                                end    
+                            else
+                                begin 
+                                    case(current_symbol)
+                                        0: begin an <= 4'b1110; seg <= 7'b010_0111; end // c
+                                        1: begin an <= 4'b1101; seg <= 7'b100_1111; end // l 
+                                        2: begin an <= 4'b1011; seg <= 7'b110_0011; end // u
+                                        default: begin an <= 4'b1001; seg <= 7'b111_0111; end // error signal
+                                    endcase      
+                                end // c
+                        end        
         end
 end
 /* ------------------- Subtask C -------------------*/
 
-always @ (clock_100hz_status_count)
-begin
-if(subtaskA_done_flag)
-    begin
-        case(clock_100hz_status_count)
-        2'b00: begin led_14 <= 1; led_13 <= 0; led_12 <= 0; end
-        2'b01: begin led_14 <= 0; led_13 <= 1; led_12 <= 0; end
-        2'b10: begin led_14 <= 0; led_13 <= 0; led_12 <= 1; end
-        default: begin led_14 <= 0; led_13 <= 0; led_12 <= 0;end
-        endcase
-    end
-end
+//always @ (clock_100hz_status_count) // debugging purposes
+//begin
+//if(subtaskA_done_flag)
+//    begin
+//        case(clock_100hz_status_count)
+//        2'b00: begin led_14 <= 1; led_13 <= 0; led_12 <= 0; end
+//        2'b01: begin led_14 <= 0; led_13 <= 1; led_12 <= 0; end
+//        2'b10: begin led_14 <= 0; led_13 <= 0; led_12 <= 1; end
+//        default: begin led_14 <= 0; led_13 <= 0; led_12 <= 0;end
+//        endcase
+//    end
+//end
 
 /* -------------------- Subtask D -------------------*/
-assign subtask_D_led = master_stop;
+//assign subtask_D_led = master_stop; // debugging purposes
 
 endmodule
